2450648826276d68252df011e6532d4f62e99e6e
losfair <zhy20000919@hotmail.com>
2020-05-21 16:09:24 +0800

    Regfile read-after-write.

--- rtl/base/regfile.v
+++ rtl/base/regfile.v
@@ -3,9 +3,16 @@ module regfile(clk, we, windex, win, rindex0, rout0, rindex1, rout1);
 input wire clk, we;
 input wire [4:0] windex, rindex0, rindex1;
 input wire [31:0] win;
-output reg [31:0] rout0, rout1;
+output wire [31:0] rout0, rout1;
 
 reg [31:0] store[31:0];
+reg [4:0] rindex0_was_written, rindex1_was_written;
+reg [31:0] win_d1;
+reg [31:0] rout0_before_write, rout1_before_write;
+
+assign rout0 = rindex0_was_written ? win_d1 : rout0_before_write;
+assign rout1 = rindex1_was_written ? win_d1 : rout1_before_write;
+
 integer i;
 
 initial begin
@@ -14,9 +21,14 @@ end
 
 always @ (posedge clk) begin
     if(we) store[windex] <= win;
-    rout0 <= store[rindex0];
-    rout1 <= store[rindex1];
+    rout0_before_write <= store[rindex0];
+    rout1_before_write <= store[rindex1];
 end
+
+always @ (posedge clk) rindex0_was_written <= we && rindex0 == windex;
+always @ (posedge clk) rindex1_was_written <= we && rindex1 == windex;
+always @ (posedge clk) win_d1 <= win;
+
 /*
 always @ (negedge clk) begin
     for(i = 0; i <= 31; i = i + 1) begin
